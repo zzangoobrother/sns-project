c2758087096dfa516934a1e01ca80edc17463f57
choiseon <com1368@naver.com>
2023-03-03 19:01:22 +0900

    feat : front-end code 추가, build.gradle 수정, deploy.yml 추가

--- src/main/java/com/example/snsproject/SnsProjectApplication.java
+++ src/main/java/com/example/snsproject/SnsProjectApplication.java
@@ -2,8 +2,9 @@ package com.example.snsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SnsProjectApplication {
 
     public static void main(String[] args) {
